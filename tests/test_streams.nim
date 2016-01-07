3039fb5ef59faac46d5ea916558e237aa182c7e9
Michał Zieliński <michal@zielinscy.org.pl>
2016-01-07 20:25:58 +0100

    fix for travis

--- tests/test_streams.nim
+++ tests/test_streams.nim
@@ -1,12 +1,14 @@
 # TEST.
-discard """read hello"""
+discard """read hello
+more X"""
 import reactor/async, reactor/loop
 
 proc main() {.async.} =
   let (stream, provider) = newStreamProviderPair[byte]()
-  let writeFut = provider.write("\0\0\0\x05hello")
+  let writeFut = provider.write("\0\0\0\x05helloX")
   let data = await stream.readChunkPrefixed()
   echo "read ", data
+  echo "more ", await stream.read(1)
   await writeFut
 
 main().ignore()
