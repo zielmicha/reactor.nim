70cec19616647cdfe5e5cd4309e6ab8366b67352
Michał Zieliński <michal@zielinscy.org.pl>
2018-04-03 12:29:45 +0200

    fix deprecation warnings in threading

--- reactor/threading.nim
+++ reactor/threading.nim
@@ -5,12 +5,12 @@ import reactor/uv/uvmultithread
 export execInPool
 export uvmultithread
 
-template maybeWrapResult(e): expr =
+template maybeWrapResult(e): untyped =
   when e is Result:
     e
   else:
     just(e)
 
-template spawn*(e: expr): expr =
+template spawn*(e: typed): untyped =
   ## Be cautionous when using this function to not capture too much, as everything captured will be copied to worker thread.
   execInPool(proc(): auto = maybeWrapResult(e))
